794e4e34a70bb493930ab6978c8ce31a728ad561
deslancer <deslancers@gmail.com>
2025-04-11 12:23:16 +0200

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Start ComfyUI in the background
-cd /ComfyUI
-nohup python main.py --listen 0.0.0.0 > /var/log/comfyui.log 2>&1 &
+# Start ComfyUI
+cd ./ComfyUI || { echo "ComfyUI directory not found"; exit 1; }
+nohup python main.py --listen 0.0.0.0 > ../var/log/comfyui.log 2>&1 &
 echo "ComfyUI started on port 8188"
 
 # Start Jupyter Lab
-cd /notebooks
+cd ../notebooks || { echo "Notebooks directory not found"; exit 1; }
 echo "Starting Jupyter Lab on port 8888"
 jupyter lab --allow-root --no-browser --ip=0.0.0.0 --port=8888
