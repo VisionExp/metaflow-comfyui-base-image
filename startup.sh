53a673738e81a33abd4001cbd536f34d7f171600
deslancer <deslancers@gmail.com>
2025-08-05 14:16:44 +0200

    jupyter removed

--- startup.sh
+++ startup.sh
@@ -4,8 +4,3 @@
 cd /home/ComfyUI || { echo "ComfyUI directory not found"; exit 1; }
 nohup python main.py --listen 0.0.0.0 > /var/log/comfyui.log 2>&1 &
 echo "ComfyUI started on port 8188"
-
-# Start Jupyter Lab
-cd /notebooks || { echo "Notebooks directory not found"; exit 1; }
-echo "Starting Jupyter Lab on port 8888"
-jupyter lab --allow-root --no-browser --ip=0.0.0.0 --port=8888
